e75788cde8cc766231f1c34bf22bfaef484b2640
Sanjib Sinha <sanjib12sinha@gmail.com>
2025-01-04 12:44:28 +0530

    second commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Map the URL '/' to the 'index' view
+    path('about/', views.about, name='about'),  # Map the URL '/about/' to the 'about' view
+    path('contact/', views.contact, name='contact'),  # Map the URL '/contact/' to the 'contact' view
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
 def index(request):
-    context = {'message': 'Hello from Django!'}  # Context is created HERE
+    context = {
+        'message': 'Hello from Django World!',
+        'items': ['Apple', 'Banana', 'Orange', 'Grapes'],
+        'author': {
+            'name': 'John Doe',
+            'age': 30,
+            'city': 'New York'
+        }
+    }
     return render(request, 'myapp/index.html', context)
+
+def about(request):
+    context = {'message': 'About page'}  # Context is created HERE
+    return render(request, 'myapp/about.html', context)
+
+def contact(request):
+    context = {'message': 'Contact page'}  # Context is created HERE
+    return render(request, 'myapp/contact.html', context)
+
